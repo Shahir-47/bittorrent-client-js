fb11d82e761f5c3c2d79f7720f8e31a18616d9a0
Shahir Ahmed <ahmeds@dickinson.edu>
2025-01-10 07:03:33 -0500

    feat: add decodeBencodeDictionary function to handle dictionary parsing in bencoded values

--- app/main.js
+++ app/main.js
@@ -7,6 +7,8 @@ const util = require("util");
 function decodeBencode(bencodedValue) {
 	if (bencodedValue[0] === "l") {
 		return decodeBencodeList(bencodedValue);
+	} else if (bencodedValue[0] === "d") {
+		return decodeBencodeDictionary(bencodedValue);
 	} else if (!isNaN(bencodedValue[0])) {
 		return decodeBencodeString(bencodedValue);
 	} else if (bencodedValue[0] === "i") {
@@ -14,6 +16,56 @@ function decodeBencode(bencodedValue) {
 	} else {
 		throw new Error("Only strings are supported at the moment");
 	}
+}
+
+function decodeBencodeDictionary(bencodedValue) {
+	const result = {};
+	let currentIndex = 1;
+
+	while (currentIndex < bencodedValue.length - 1) {
+		const colonIndex = bencodedValue.indexOf(":", currentIndex);
+		const stringLength = parseInt(
+			bencodedValue.slice(currentIndex, colonIndex)
+		);
+		const key = decodeBencodeString(
+			bencodedValue.slice(colonIndex, colonIndex + stringLength + 1)
+		);
+		currentIndex = colonIndex + stringLength + 1;
+
+		if (bencodedValue[currentIndex] === "i") {
+			const endIndex = bencodedValue.indexOf("e", currentIndex) + 1;
+			result[key] = decodeBencodeInt(
+				bencodedValue.slice(currentIndex, endIndex)
+			);
+			currentIndex = endIndex;
+		} else if (!isNaN(bencodedValue[currentIndex])) {
+			const colonIndex = bencodedValue.indexOf(":", currentIndex);
+			const stringLength = parseInt(
+				bencodedValue.slice(currentIndex, colonIndex)
+			);
+			result[key] = decodeBencodeString(
+				bencodedValue.slice(colonIndex, colonIndex + stringLength + 1)
+			);
+			currentIndex = colonIndex + stringLength + 1;
+		} else if (bencodedValue[currentIndex] === "l") {
+			let endIndex = bencodedValue.lastIndexOf("e") - 1;
+
+			while (!isNaN(bencodedValue[endIndex - 1]) && endIndex > 0) {
+				endIndex = bencodedValue.lastIndexOf("e", endIndex - 1);
+			}
+
+			result[key] = decodeBencodeList(
+				bencodedValue.slice(currentIndex, endIndex + 1)
+			);
+
+			currentIndex = endIndex + 1;
+		} else {
+			throw new Error(
+				"Only integers, strings and lists are supported at the moment"
+			);
+		}
+	}
+	return result;
 }
 
 function decodeBencodeList(bencodedValue) {
